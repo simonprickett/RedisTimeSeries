3f8a170b567f452209c0c9af994ed6291d1cd46c
Ofir Moskovich <ofir.moskovich@redislabs.com>
2021-09-29 12:15:21 +0300

    optimized count trailing zero function

--- deps/dragonbox/dragonbox.cpp
+++ deps/dragonbox/dragonbox.cpp
@@ -3104,16 +3104,17 @@ namespace jkj::dragonbox {
         }
 
         constexpr inline auto digit_count_table = generate_digit_count_table();
-
+/*
         static inline int floor_log2(std::uint64_t n) noexcept {
             return 63 ^ __builtin_clzll(n);
         }
-
+        */
+/*
         static inline int count_digit(std::uint64_t n) noexcept {
             return int((digit_count_table.entry[floor_log2(n)] + (n >> (floor_log2(n) / 4))) >> 52);
         }
-
-        JKJ_FORCEINLINE static void convert_64_simple(char* const buffer, int buf_ind_start, int buf_ind_end, std::uint64_t &value)
+*/
+        void convert_64_simple(char* const buffer, int buf_ind_start, int buf_ind_end, std::uint64_t &value)
         {
             while (buf_ind_end >= buf_ind_start)
             {
@@ -3123,7 +3124,7 @@ namespace jkj::dragonbox {
             }
         }
 
-        JKJ_FORCEINLINE static void convert_64(char* const buffer, int buf_ind_start, int buf_ind_end, std::uint64_t &value)
+        void convert_64(char* const buffer, int buf_ind_start, int buf_ind_end, std::uint64_t &value)
         {
             while (buf_ind_end >= buf_ind_start + 3)
             {
@@ -3330,8 +3331,89 @@ namespace jkj::dragonbox {
             return buffer;
         }
 
+        constexpr std::uint64_t __masks[] = {
+            0x0ull,
+            0x1ull,
+            0x3ull,
+            0x7ull,
+            0xFull,
+            0x1Full,
+            0x3Full,
+            0x7Full,
+            0xFFull,
+            0x1FFull,
+            0x3FFull,
+            0x7FFull,
+            0xFFFull,
+            0x1FFFull,
+            0x3FFFull,
+            0x7FFFull,
+            0xFFFFull,
+            0x1FFFFull,
+            0x3FFFFull,
+            0x7FFFFull
+        };
+
+        constexpr std::uint64_t MULINV_POW5[20][2] = {
+            { 0ull,                          // unused value
+              0                         },   // unused value
+            { 14757395258967641293ull,                 // modular multiplicative inverse of 5
+            (0xFFFF'FFFF'FFFF'FFFF)/5ull},   // condition: floor((2^64 - 1)/5)
+            { 10330176681277348905ull,                 // modular multiplicative inverse of 5^2
+            (0xFFFF'FFFF'FFFF'FFFF)/25ull},  // condition: floor((2^64 - 1)/(5^2))
+            { 2066035336255469781ull,                  // modular multiplicative inverse of 5^3
+            (0xFFFF'FFFF'FFFF'FFFF)/125ull},  // condition: floor((2^64 - 1)/(5^3))
+            { 15170602326218735249ull,                  // modular multiplicative inverse of 5^4
+            (0xFFFF'FFFF'FFFF'FFFF)/625ull},  // condition: floor((2^64 - 1)/(5^4))
+            { 6723469279985657373ull,                  // modular multiplicative inverse of 5^5
+            (0xFFFF'FFFF'FFFF'FFFF)/3125ull},  // condition: floor((2^64 - 1)/(5^5))
+            { 8723391485480952121ull,                  // modular multiplicative inverse of 5^6
+            (0xFFFF'FFFF'FFFF'FFFF)/15625ull},  // condition: floor((2^64 - 1)/(5^6))
+            { 16502073556063831717ull,                  // modular multiplicative inverse of 5^7
+            (0xFFFF'FFFF'FFFF'FFFF)/78125ull},  // condition: floor((2^64 - 1)/(5^7))
+            { 14368461155438497313ull,                  // modular multiplicative inverse of 5^8
+            (0xFFFF'FFFF'FFFF'FFFF)/390625ull},  // condition: floor((2^64 - 1)/(5^8))
+            { 10252389860571520109ull,                  // modular multiplicative inverse of 5^9
+            (0xFFFF'FFFF'FFFF'FFFF)/1953125ull},  // condition: floor((2^64 - 1)/(5^9))
+            { 5739826786856214345ull,                  // modular multiplicative inverse of 5^10
+            (0xFFFF'FFFF'FFFF'FFFF)/9765625ull},  // condition: floor((2^64 - 1)/(5^10))
+            { 1147965357371242869ull,                  // modular multiplicative inverse of 5^11
+            (0xFFFF'FFFF'FFFF'FFFF)/48828125ull},  // condition: floor((2^64 - 1)/(5^11))
+            { 3918941886216158897ull,                  // modular multiplicative inverse of 5^12
+            (0xFFFF'FFFF'FFFF'FFFF)/244140625ull},  // condition: floor((2^64 - 1)/(5^12))
+            { 11851834821468962749ull,                  // modular multiplicative inverse of 5^13
+            (0xFFFF'FFFF'FFFF'FFFF)/1220703125ull},  // condition: floor((2^64 - 1)/(5^13))
+            { 6059715779035702873ull,                  // modular multiplicative inverse of 5^14
+            (0xFFFF'FFFF'FFFF'FFFF)/6103515625ull},  // condition: floor((2^64 - 1)/(5^14))
+            { 8590640785290961221ull,                  // modular multiplicative inverse of 5^15
+            (0xFFFF'FFFF'FFFF'FFFF)/30517578125ull},  // condition: floor((2^64 - 1)/(5^15))
+            { 16475523416025833537ull,                  // modular multiplicative inverse of 5^16
+            (0xFFFF'FFFF'FFFF'FFFF)/152587890625ull},  // condition: floor((2^64 - 1)/(5^16))
+            { 14363151127430897677ull,                  // modular multiplicative inverse of 5^17
+            (0xFFFF'FFFF'FFFF'FFFF)/762939453125ull},  // condition: floor((2^64 - 1)/(5^17))
+            { 13940676669711910505ull,                  // modular multiplicative inverse of 5^18
+            (0xFFFF'FFFF'FFFF'FFFF)/3814697265625ull},  // condition: floor((2^64 - 1)/(5^18))
+            { 2788135333942382101ull,                  // modular multiplicative inverse of 5^19
+            (0xFFFF'FFFF'FFFF'FFFF)/19073486328125ull},  // condition: floor((2^64 - 1)/(5^19))
+        };
+
+        // Inspired by Hackers Delight
+        int ctzll_base10_fastest(std::uint64_t x, int n_digits_minus_1) {
+            std::uint64_t q;
+            while(n_digits_minus_1 > 0) {
+                q = x*MULINV_POW5[n_digits_minus_1][0]; // x*(multiplicative inverse of 5^n_digits_minus_1) (mod 2^64)
+                //printf("q=%lu, x=%lu, MULINV_POW5=%lu, n_digits_minus_1=%d\n", q, x, MULINV_POW5[n_digits_minus_1][0], n_digits_minus_1);
+                if(q <= MULINV_POW5[n_digits_minus_1][1] && ((q & __masks[n_digits_minus_1]) == 0)) {
+                    // q <= floor(2^64 - 1) and q ends in k or more 0-bits
+                    return n_digits_minus_1;
+                }
+                n_digits_minus_1 -= 1;
+            }
+            return 0;
+        }
+
         // assumes x > 0
-        JKJ_FORCEINLINE static int ctzll_base10_simple(std::uint64_t x, int n_digits_minus_1) {
+        int ctzll_base10_simple(std::uint64_t x, int n_digits_minus_1) {
             while(n_digits_minus_1 > 0) {
                 if(x%drag__pow10[n_digits_minus_1] == 0) {
                     return n_digits_minus_1;
@@ -3341,7 +3423,7 @@ namespace jkj::dragonbox {
             return 0;
         }
 
-        JKJ_FORCEINLINE static int ctzll_base10(std::uint64_t x, int n_digits_minus_1) {
+        int ctzll_base10(std::uint64_t x, int n_digits_minus_1) {
             while(n_digits_minus_1 > 5) {
                 if(x%drag__pow10[n_digits_minus_1] == 0) {
                     return n_digits_minus_1;
@@ -3373,7 +3455,7 @@ namespace jkj::dragonbox {
             return 0;
         }
 
-        JKJ_FORCEINLINE static constexpr std::uint32_t
+        std::uint32_t
         decimal_length_u64(std::uint64_t const v) {
             assert(v < 10'0000'0000'0000'0000);
             if (v >= 1'0000'0000'0000'0000) {
@@ -3449,7 +3531,7 @@ namespace jkj::dragonbox {
                 if(n_digits + exponent > 0) {
                     // in this case there is no need for the exponent cause the number can fit in 17 chars
                     //no_exponent = true;
-                    int n_trailing_zeros = ctzll_base10_simple(significand, n_digits - 1);
+                    int n_trailing_zeros = ctzll_base10_fastest(significand, n_digits - 1);
                     if(n_trailing_zeros + exponent >= 0) {
                         significand /= drag__pow10[(-1)*exponent];
                         n_digits += exponent;
@@ -3740,3 +3822,17 @@ void dragonbox_double_to_chars(double x, char *buffer) {
     jkj::dragonbox::to_chars(x, buffer);
 }
 }
+
+/*
+int main(int argc, char *argv[]) {
+    char buf[25];
+    //jkj::dragonbox::to_chars(4.5, buf);
+    //printf("buf=%s\n", buf);
+
+    for(int i = 0; i<1000000; i++) {
+        jkj::dragonbox::to_chars(7.6773, buf);
+        jkj::dragonbox::to_chars(7.5284, buf);
+        jkj::dragonbox::to_chars(6.82, buf);
+    }
+}
+*/
